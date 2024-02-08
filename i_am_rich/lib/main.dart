0a98ae9d47d4186975cccefefea7769b3c6220ff
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2024-02-07 23:20:16 -0300

    i_am_rich test app

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('I Am Rich'),
-          backgroundColor: Colors.lightBlueAccent,
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: const Text('I Am Rich'),
+        titleTextStyle: const TextStyle(color: Colors.white, fontSize: 20),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: const Center(
+        child: Image(
+          image: NetworkImage(
+              'https://passiveincomemd.com/wp-content/uploads/2017/05/rich-vs-wealthy-1.jpg'),
         ),
       ),
-    )
-  );
+    ),
+  ));
 }
