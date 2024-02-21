a1035cb5aa7afb1533ec6b275145c586e7604271
Luiz Fernando <luizfernandodematoscarvalho@gmail.com>
2024-02-20 23:39:26 -0300

    mi card beta

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -9,38 +9,91 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.white12,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                width: 100,
-                color: Colors.red,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  ),
-                ],
-              ),
-              Container(
-                width: 100,
-                color: Colors.blue,
-              ),
-            ],
+            child: Column(children: [
+          CircleAvatar(
+            radius: 50,
+            backgroundColor: Colors.white10,
+            backgroundImage: AssetImage('images/profile_icon.png'),
           ),
-        ),
+          Text(
+            'Luiz Fernando',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'KodeMono'),
+          ),
+          Text(
+            'PROGRAMMING STUDENT',
+            style: TextStyle(
+                color: Colors.white38,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'SourceSans3',
+                letterSpacing: 2.5),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 1,
+            width: 100,
+            color: Colors.white38,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            color: Colors.white38,
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            padding: EdgeInsets.all(10),
+            child: const Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'luizfernandodematoscarvalho@gmail.com',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'SourceSans3'),
+                )
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.white38,
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            padding: EdgeInsets.all(10),
+            child: const Row(
+              children: [
+                Icon(
+                  Icons.phone,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  '+55 11 98099-1245',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'SourceSans3'),
+                )
+              ],
+            ),
+          )
+        ])),
       ),
     );
   }
